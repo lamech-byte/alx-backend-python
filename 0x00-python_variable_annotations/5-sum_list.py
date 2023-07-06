8f3effe698271778d2f4c0b5dd60f0fdd5a8487c
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-07-06 15:28:20 +0300

    5-sum_list.py

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,7 +2,8 @@
 
 """
 Module: 5-sum_list
-Contains a type-annotated function `sum_list` that takes a list of floats as an argument and returns their sum as a float.
+Contains a type-annotated function `sum_list` that takes a list of floats
+as an argument and returns their sum as a float.
 """
 
 from typing import List

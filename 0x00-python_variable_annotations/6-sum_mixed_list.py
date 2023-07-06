d956fd812c8ac71145f5d0d96301b4b2f9f965ec
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-07-06 10:38:32 +0300

    6-sum_mixed_list.py

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+Module: 6-sum_mixed_list
+Contains a type-annotated function `sum_mixed_list` that takes a list of integers and floats and returns their sum as a float.
+"""
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    Function: sum_mixed_list
+    Takes a list of integers and floats as an argument and returns their sum as a float.
+    """
+    return sum(mxd_lst)

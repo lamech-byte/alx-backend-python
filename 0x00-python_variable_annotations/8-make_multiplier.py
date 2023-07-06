79a8a5b56712b626cb5a5fae9d01c48017a5cbf1
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-07-06 10:44:39 +0300

    8-make_multiplier.py

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+"""
+Module: 8-make_multiplier
+Contains a type-annotated function `make_multiplier` that takes a float as an argument and returns a function.
+The returned function multiplies a float by the given multiplier.
+"""
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Function: make_multiplier
+    Takes a float as an argument and returns a function.
+    The returned function multiplies a float by the given multiplier.
+    """
+    def multiply(n: float) -> float:
+        return n * multiplier
+
+    return multiply

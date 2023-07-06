6b1e59ad0160bdb7c13e2438754fa6ca7e063351
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-07-06 09:58:46 +0300

    1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+Module: 1-concat
+Contains a type-annotated function `concat` that takes two strings as arguments and returns their concatenated string.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Function: concat
+    Takes two strings as arguments and returns their concatenated string.
+    """
+    return str1 + str2

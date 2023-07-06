2dca81a3aea3e6b8b992a6d85ee22bf8e6386a6d
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-07-06 09:51:42 +0300

    0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+Module: 0-add
+Contains a type-annotated function `add` that takes two floats as arguments and returns their sum as a float.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Function: add
+    Takes two floats as arguments and returns their sum as a float.
+    """
+    return a + b

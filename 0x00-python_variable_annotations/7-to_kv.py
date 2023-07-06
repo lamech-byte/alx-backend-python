c3a5e11c807e371b51f619f4c0faf9789ce10c9b
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-07-06 15:29:10 +0300

    7-to_kv.py

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -2,8 +2,10 @@
 
 """
 Module: 7-to_kv
-Contains a type-annotated function `to_kv` that takes a string and an int/float as arguments and returns a tuple.
-The first element of the tuple is the string. The second element is the square of the int/float as a float.
+Contains a type-annotated function `to_kv` that takes a string
+and an int/float as arguments and returns a tuple.
+The first element of the tuple is the string. The second element
+is the square of the int/float as a float.
 """
 
 from typing import Union, Tuple

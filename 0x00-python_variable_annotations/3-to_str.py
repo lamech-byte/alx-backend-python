c9e89ceb935fc5141e4c0acdc2c729f964cd3e0b
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-07-06 10:07:47 +0300

    3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+Module: 3-to_str
+Contains a type-annotated function `to_str` that takes a float as an argument and returns its string representation.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Function: to_str
+    Takes a float as an argument and returns its string representation.
+    """
+    return str(n)

45f160e2d764966c59d3adf53c9e3c9c4ca84902
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-07-06 10:52:51 +0300

    100-safe_first_element.py

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+Module: 100-safe_first_element
+Contains a duck-typed function `safe_first_element` that safely retrieves the first element from a sequence.
+"""
+
+from typing import Sequence, Any, Union
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """
+    Function: safe_first_element
+    Takes a sequence as an argument and returns the first element if it exists, otherwise returns None.
+    """
+    if lst:
+        return lst[0]
+    else:
+        return None

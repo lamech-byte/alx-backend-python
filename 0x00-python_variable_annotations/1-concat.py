d437d829581a3b748c1c6acaf1bdf946c6a2d6a0
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-07-06 15:11:47 +0300

    1-concat.py

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -10,5 +10,12 @@ def concat(str1: str, str2: str) -> str:
     """
     Function: concat
     Takes two strings as arguments and returns their concatenated string.
+
+    Args:
+        str1 (str): The first string.
+        str2 (str): The second string.
+
+    Returns:
+        str: The concatenated string of str1 and str2.
     """
     return str1 + str2

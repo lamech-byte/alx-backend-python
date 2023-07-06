7db4f09b60a9e7c801f38cf41a333976a70c0eb3
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-07-06 10:03:10 +0300

    2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+Module: 2-floor
+Contains a type-annotated function `floor` that takes a float as an argument and returns its floor as an integer.
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Function: floor
+    Takes a float as an argument and returns its floor as an integer.
+    """
+    return math.floor(n)

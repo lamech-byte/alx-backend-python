48fa173e439ce7f2d14556deeb5e508623d0a4b9
Lamech M <gesachmoh@gmail.com>
2023-07-11 14:07:46 +0300

    test

--- 0x02-python_async_comprehension/0-main.py
+++ 0x02-python_async_comprehension/0-main.py
@@ -6,6 +6,9 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def print_yielded_values():
+    """
+    Prints the values yielded by async_generator.
+    """
     result = []
     async for i in async_generator():
         result.append(i)

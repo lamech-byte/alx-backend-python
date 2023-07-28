789da0a7b4e5dc314d33954d4485e0a9c0ce2131
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-07-28 15:50:48 +0300

    test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -41,15 +41,15 @@ class TestGetJson(unittest.TestCase):
     @patch('utils.requests.get')
     def test_get_json(self, test_url, test_payload, mock_get):
         """Test get_json function"""
-        # Configure the Mock object to return test_payload when json() is called
+        # Configure the Mock object to return test_payload when json()
         mock_get.return_value = Mock()
         mock_get.return_value.json.return_value = test_payload
 
         # Call the get_json function with the test URL
         result = get_json(test_url)
 
-        # Assert that the mocked requests.get method was called exactly once with the test URL
+        # Assert that the mocked requests.get method with the test URL
         mock_get.assert_called_once_with(test_url)
 
-        # Assert that the output of get_json is equal to the expected test_payload
+        # Assert that the output of get_json is equal to the test_payload
         self.assertEqual(result, test_payload)

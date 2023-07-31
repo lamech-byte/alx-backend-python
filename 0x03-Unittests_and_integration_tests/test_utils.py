e17cfd00a05d1c8316c27f5a7d2ac3f8d98ea03d
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-07-31 21:09:51 +0300

    test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -113,5 +113,29 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(result, expected_result)
 
 
+class TestGithubOrgClient(unittest.TestCase):
+    @parameterized.expand([
+        ("google", {"payload": True}),
+        ("abc", {"payload": False})
+    ])
+    @patch('client.GithubOrgClient.get_json')  # Patch get_json method of GithubOrgClient class
+    def test_org(self, org_name, expected_result, mock_get_json):
+        """Test GithubOrgClient.org method"""
+        # Configure the mock_get_json to return the expected result
+        mock_get_json.return_value = expected_result
+
+        # Create an instance of GithubOrgClient with the org_name
+        client = GithubOrgClient(org_name)
+
+        # Call the org method
+        result = client.org
+
+        # Assert that get_json was called once with the correct argument
+        mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
+
+        # Assert that the result is equal to the expected_result
+        self.assertEqual(result, expected_result)
+
+
 if __name__ == '__main__':
     unittest.main()

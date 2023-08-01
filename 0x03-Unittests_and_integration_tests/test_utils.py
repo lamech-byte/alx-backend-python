7fc11f8393ad172562d6c5b19d988f548c814af2
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-01 11:49:26 +0300

    test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
-"""Unit tests for utils.access_nested_map, utils.get_json, and utils.memoize"""
+"""Unit tests for utils.access_nested_map and utils.get_json"""
 import unittest
 from parameterized import parameterized
 from unittest.mock import patch, Mock
 from utils import access_nested_map, get_json
-from client import GithubOrgClient
 
 
 class TestAccessNestedMap(unittest.TestCase):
+    """Test class for access_nested_map function"""
+
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
@@ -31,11 +32,13 @@ class TestAccessNestedMap(unittest.TestCase):
 
 
 class TestGetJson(unittest.TestCase):
+    """Test class for get_json function"""
+
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False})
     ])
-    @patch('client.requests.get')
+    @patch('utils.requests.get')
     def test_get_json(self, test_url, test_payload, mock_get):
         """Test get_json function"""
         # Configure the Mock object to return test_payload when json()
@@ -45,98 +48,11 @@ class TestGetJson(unittest.TestCase):
         # Call the get_json function with the test URL
         result = get_json(test_url)
 
-        # Assert that the mocked requests.get method was called exactly once
+        # Assert that the mocked requests.get method was called exactly
         mock_get.assert_called_once_with(test_url)
 
         # Assert that the output of get_json is equal to the expected
         self.assertEqual(result, test_payload)
-
-
-class TestClass:
-    def a_method(self):
-        return 42
-
-    _a_property = None
-
-    @property
-    def a_property(self):
-        if self._a_property is None:
-            self._a_property = self.a_method()
-        return self._a_property
-
-
-class TestMemoize(unittest.TestCase):
-    @patch.object(TestClass, 'a_method')
-    def test_memoize(self, mock_a_method):
-        """Test the memoize decorator"""
-        # Configure the mock_a_method to return a specific value when called
-        mock_a_method.return_value = 42
-
-        # Create an instance of TestClass
-        instance = TestClass()
-
-        # Call the a_property method twice
-        result1 = instance.a_property
-        result2 = instance.a_property
-
-        # Assert that the mock_a_method was called exactly once
-        mock_a_method.assert_called_once()
-
-        # Assert that the results of both calls are the same (memoized result)
-        self.assertEqual(result1, 42)
-        self.assertEqual(result2, 42)
-
-
-class TestGithubOrgClient(unittest.TestCase):
-    @parameterized.expand([
-        ("google", {"payload": True}),
-        ("abc", {"payload": False})
-    ])
-    @patch.object(GithubOrgClient, 'get_json')
-    def test_org(self, org_name, expected_result, mock_get_json):
-        """Test GithubOrgClient.org method"""
-        # Configure the mock_get_json to return the expected result
-        mock_get_json.return_value = expected_result
-
-        # Create an instance of GithubOrgClient
-        client = GithubOrgClient(org_name)
-
-        # Call the org method
-        result = client.org
-
-        # Assert that get_json was called once with the correct argument
-        mock_get_json.assert_called_once_with(
-            f"https://api.github.com/orgs/{org_name}"
-        )
-
-        # Assert that the result is equal to the expected_result
-        self.assertEqual(result, expected_result)
-
-
-class TestGithubOrgClient(unittest.TestCase):
-    @parameterized.expand([
-        ("google", {"payload": True}),
-        ("abc", {"payload": False})
-    ])
-    @patch('requests.get')  # Patch requests.get method
-    def test_org(self, org_name, expected_result, mock_get):
-        """Test GithubOrgClient.org method"""
-        # Configure the mock_get to return the expected result
-        mock_get.return_value.json.return_value = expected_result
-
-        # Create an instance of GithubOrgClient with the org_name
-        client = GithubOrgClient(org_name)
-
-        # Call the org method
-        result = client.org
-
-        # Assert that get_json was called once with the correct argument
-        mock_get.assert_called_once_with(
-            f"https://api.github.com/orgs/{org_name}"
-        )
-
-        # Assert that the result is equal to the expected_result
-        self.assertEqual(result, expected_result)
 
 
 if __name__ == '__main__':

708f233b193444238236f1912fc85fa0d234805c
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-01 12:26:53 +0300

    test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,6 +2,7 @@ import unittest
 from unittest.mock import patch, MagicMock
 from client import GithubOrgClient
 
+
 class TestGithubOrgClient(unittest.TestCase):
     """Test class for GithubOrgClient"""
 
@@ -13,7 +14,9 @@ class TestGithubOrgClient(unittest.TestCase):
     def test_org(self, org_name, mock_get_json):
         """Test GithubOrgClient.org method"""
         # Mock the return value of get_json to a known payload
-        org_payload = {"repos_url": f"https://api.github.com/orgs/{org_name}/repos"}
+        org_payload = {
+            "repos_url": f"https://api.github.com/orgs/{org_name}/repos"
+        }
         mock_get_json.return_value = org_payload
 
         # Initialize the GithubOrgClient with the org_name
@@ -23,10 +26,13 @@ class TestGithubOrgClient(unittest.TestCase):
         result = client.org()
 
         # Assert that get_json was called once with the correct argument
-        mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
+        mock_get_json.assert_called_once_with(
+            f"https://api.github.com/orgs/{org_name}"
+        )
 
         # Assert that the result is equal to the expected org_payload
         self.assertEqual(result, org_payload)
+
 
 if __name__ == '__main__':
     unittest.main()

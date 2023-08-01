2690063b5310344a9a8b189c0149114d25487eb3
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-01 12:34:27 +0300

    test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,38 +1,35 @@
 import unittest
-from unittest.mock import patch, MagicMock
-from parameterized import parameterized
+from unittest.mock import patch
 from client import GithubOrgClient
+from parameterized import parameterized, parameterized_class
 
 
+@parameterized_class([
+    ("google",),
+    ("abc",),
+])
 class TestGithubOrgClient(unittest.TestCase):
     """Test class for GithubOrgClient"""
 
-    @parameterized.expand([
-        ("google",),
-        ("abc",),
-    ])
     @patch('client.get_json')
     def test_org(self, org_name, mock_get_json):
         """Test GithubOrgClient.org method"""
-        # Mock the return value of get_json to a known payload
-        org_payload = {
-            "repos_url": f"https://api.github.com/orgs/{org_name}/repos"
-        }
-        mock_get_json.return_value = org_payload
+        expected_result = {"repos_url": f"https://api.github.com/orgs/{org_name}/repos"}
 
-        # Initialize the GithubOrgClient with the org_name
+        # Configure the mock_get_json to return the expected_result
+        mock_get_json.return_value = expected_result
+
+        # Create an instance of GithubOrgClient
         client = GithubOrgClient(org_name)
 
         # Call the org method
         result = client.org()
 
         # Assert that get_json was called once with the correct argument
-        mock_get_json.assert_called_once_with(
-            f"https://api.github.com/orgs/{org_name}"
-        )
+        mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
 
-        # Assert that the result is equal to the expected org_payload
-        self.assertEqual(result, org_payload)
+        # Assert that the result is the same as the expected_result
+        self.assertEqual(result, expected_result)
 
 
 if __name__ == '__main__':

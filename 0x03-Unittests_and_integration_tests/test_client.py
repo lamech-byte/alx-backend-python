649c56016a80d746851c73165e0e9a2b5f325ef1
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-01 12:39:16 +0300

    test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,39 +2,40 @@
 import unittest
 from unittest.mock import patch
 from client import GithubOrgClient
-from parameterized import parameterized, parameterized_class
 
 
-@parameterized_class([
-    ("google",),
-    ("abc",),
-])
 class TestGithubOrgClient(unittest.TestCase):
     """Test class for GithubOrgClient"""
 
-    @patch('client.get_json')
-    def test_org(self, org_name, mock_get_json):
+    def test_org(self):
         """Test GithubOrgClient.org method"""
-        expected_result = {
-            "repos_url": f"https://api.github.com/orgs/{org_name}/repos"
-        }
+        org_names = ["google", "abc"]
+        expected_urls = [
+            "https://api.github.com/orgs/google/repos",
+            "https://api.github.com/orgs/abc/repos",
+        ]
 
-        # Configure the mock_get_json to return the expected_result
-        mock_get_json.return_value = expected_result
+        for i in range(len(org_names)):
+            org_name = org_names[i]
+            expected_url = expected_urls[i]
 
-        # Create an instance of GithubOrgClient
-        client = GithubOrgClient(org_name)
+            # Patch get_json and return the expected URL
+            with patch('client.get_json') as mock_get_json:
+                mock_get_json.return_value = {"repos_url": expected_url}
 
-        # Call the org method
-        result = client.org()
+                # Create an instance of GithubOrgClient
+                client = GithubOrgClient(org_name)
 
-        # Assert that get_json was called once with the correct argument
-        mock_get_json.assert_called_once_with(
-            f"https://api.github.com/orgs/{org_name}"
-        )
+                # Call the org method
+                result = client.org()
 
-        # Assert that the result is the same as the expected_result
-        self.assertEqual(result, expected_result)
+                # Assert that get_json was called once with the correct argument
+                mock_get_json.assert_called_once_with(
+                    f"https://api.github.com/orgs/{org_name}"
+                )
+
+                # Assert that the result is the same as the expected URL
+                self.assertEqual(result, expected_url)
 
 
 if __name__ == '__main__':

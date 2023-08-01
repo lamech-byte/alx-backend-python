1391f9dd551c44b938ba2cb9f0651a69c0d88948
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-01 20:01:42 +0300

    test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """Unit tests for client.GithubOrgClient.org method"""
 import unittest
 from parameterized import parameterized
-from unittest.mock import patch, Mock
+from unittest.mock import patch, PropertyMock
 from client import GithubOrgClient
 
 

ad26f137591baedaa5a0a4ae389486e37f955484
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-01 13:00:32 +0300

    test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,53 +1,37 @@
 #!/usr/bin/env python3
+"""Unit tests for client.GithubOrgClient.org method"""
 import unittest
-from unittest.mock import MagicMock
 from parameterized import parameterized
-
+from unittest.mock import patch, Mock
 from client import GithubOrgClient
 
 
 class TestGithubOrgClient(unittest.TestCase):
-    def setUp(self):
-        self.get_json_patch = unittest.mock.patch('client.get_json')
-        self.mock_get_json = self.get_json_patch.start()
-        self.mock_get_json.return_value = {"payload": True}
-
-    def tearDown(self):
-        self.get_json_patch.stop()
+    """Test class for GithubOrgClient.org method"""
 
     @parameterized.expand([
-        ("google",),
-        ("abc",),
+        ("google", {"payload": True}),
+        ("abc", {"payload": False})
     ])
-    def test_org(self, org_name):
+    @patch('client.get_json')  # Patch get_json method
+    def test_org(self, org_name, expected_result, mock_get_json):
         """Test GithubOrgClient.org method"""
+        # Configure the mock_get_json to return the expected result
+        mock_get_json.return_value = expected_result
+
         # Create an instance of GithubOrgClient
         client = GithubOrgClient(org_name)
 
-        # Access the org property without calling it
+        # Call the org method
         result = client.org
 
-        # Assert that the result is not None
-        self.assertIsNotNone(result)
+        # Assert that get_json was called once with the correct argument
+        mock_get_json.assert_called_once_with(
+            f"https://api.github.com/orgs/{org_name}"
+        )
 
-        # Assert that get_json was called with the correct URL
-        expected_url = f"https://api.github.com/orgs/{org_name}"
-        self.mock_get_json.assert_called_once_with(expected_url)
-
-    @parameterized.expand([
-        ("google", "https://api.github.com/orgs/google/repos"),
-        ("abc", "https://api.github.com/orgs/abc/repos"),
-    ])
-    def test_public_repos_url(self, org_name, expected_url):
-        """Test GithubOrgClient._public_repos_url property"""
-        # Create an instance of GithubOrgClient
-        client = GithubOrgClient(org_name)
-
-        # Access the _public_repos_url property
-        result = client._public_repos_url
-
-        # Assert that the result matches the expected URL
-        self.assertEqual(result, expected_url)
+        # Assert that the result is equal to the expected_result
+        self.assertEqual(result, expected_result)
 
 
 if __name__ == '__main__':
